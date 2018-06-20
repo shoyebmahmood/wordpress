d5deccc87f399ba302601af1bac779cf0daac3e8
shoyebmahmood <mahmoodshoyeb@gmail.com>
2018-06-19 22:23:25 -0700

    new start

--- functions.php
+++ functions.php
@@ -1,35 +1,14 @@
 <?php
 function shoyeb_bootstrapping(){
     load_theme_textdomain("shoyeb");
+    add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
-    add_theme_support("post_thumbnails");
-
 }
 add_action("after_setup_theme","shoyeb_bootstrapping");
+
 function shoyeb_assets(){
     wp_enqueue_style("shoyeb",get_stylesheet_uri());
     wp_enqueue_style("bootstrap","//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 add_action("wp_enqueue_scripts","shoyeb_assets");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 
 <head>
     <meta charset="UTF-8">
-    <?php wp_head();?>
+    <?php wp_head(); ?>
 </head>
 
-<body <?php body_class();?>>
+<body <?php body_class(); ?>>
     <div class="header">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <h3 class="tagline">
-                        <?php bloginfo("description");?>
+                        <?php bloginfo("description"); ?>
                     </h3>
                     <h1 class="align-self-center display-1 text-center heading">
-                        <?php bloginfo("name");?>
+                        <?php bloginfo("name"); ?>
                     </h1>
                 </div>
             </div>
@@ -23,50 +23,48 @@
     </div>
     <div class="posts">
         <?php
-        while(have_posts()){
-    the_post();
-            ?>
-            <div class="post" <?php post_class();?>>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h2 class="post-title">
-                                <?php the_title();?>
-                            </h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <p>
-                                <strong><?php the_author();?></strong><br/>
-                                <?php the_date();?>
-
-                            </p>
-                            <?php
-            echo get_the_tag_list("<ul class=\"list-unstyled\"><li>","</li><li>","</li></ul>");
-            ?>
-
-                        </div>
-                        <div class="col-md-8">
-                            <p>
-                                <?php
-            if(has_post_thumbnail()){
-                the_post_thumbnail("large","class=>'img-fluid'");
-            }
+            while(have_posts()){
+                the_post();
                 ?>
-                            </p>
-                            <?php the_content();?>
+        <div class="post" <?php post_class(); ?>>
 
-                        </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="post-title">
+                            <?php the_title(); ?>
+                        </h2>
                     </div>
-
                 </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <p>
+                            <strong><?php the_author(); ?></strong><br/>
+                            <?php the_date(); ?>
+                        </p>
+                        <ul class="list-unstyled">
+                            <li>dhaka</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-8">
+                        <p>
+                            <?php
+                if(has_post_thumbnail()){
+                    the_post_thumbnail("large","class='img-fluid'");
+                }
+
+                            ?>
+                        </p>
+                        <?php the_content();?>
+                    </div>
+                </div>
+
             </div>
+        </div>
 
-
-            <?php
-}
-    ?>
+        <?php
+            }
+            ?>
 
 
 
@@ -75,12 +73,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; shoyebmahmood.
+                    &copy; shoyebmahmood - All Rights Reserved
                 </div>
             </div>
         </div>
     </div>
-    <?php wp_footer();?>
+    <?php wp_footer(); ?>
 </body>
 
 </html>
