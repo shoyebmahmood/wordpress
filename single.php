42fdff02015aef5817d9cbed09711b960fe5b82d
shoyebmahmood <mahmoodshoyeb@gmail.com>
2018-06-22 21:57:35 +0600

    change

--- single.php
+++ single.php
@@ -11,34 +11,38 @@
 
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12">
-                            <h2 class="post-title">
-                                <a href="<?php the_permalink();?>">
-                                    <?php the_title(); ?>
-                                </a>
+
+                        <div class="col-md-10 offset-md-1">
+                            <h2 class="post-title text-center">
+
+                                <?php the_title(); ?>
+
                             </h2>
+                            <p class="text-center">
+                                <strong><?php the_author(); ?></strong><br/>
+                                <?php the_date(); ?>
+                            </p>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-4">
-                            <p>
-                                <strong><?php the_author(); ?></strong><br/>
-                                <?php echo get_the_date(); ?>
-                            </p>
-                            <?php echo get_the_tag_list("<ul class=\"list-unstyled\"><li>","</li><li>","</li></ul>");?>
 
-                        </div>
-                        <div class="col-md-8">
+                        <div class="col-md-10 offset-md-1">
                             <p>
                                 <?php
                 if(has_post_thumbnail()){
                     the_post_thumbnail("large","class='img-fluid'");
                 }
 ?>
+
+                                    <?php the_content();?>
                             </p>
-                            <?php the_excerpt();
-                ?>
                         </div>
+                        <?php if (comments_open()):?>
+                        <div class="col-md-10-offset-md-1">
+                            <?php comments_template();?>
+                        </div>
+                        <?php endif;?>
+
                     </div>
 
                 </div>
